f9dd5f2a948b83c9bd059cd4d88c7b9ffe6bb36c
wendoli mosquea <wendolimosquea@gmail.com>
2025-02-20 22:26:09 -0400

    Optimizando el codigo

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombreDeAmigo;
 let listaDeAmigos = [];
+let boton_sortear = document.getElementById('boton_sortear');
+let boton_reinicio = document.getElementById('reiniciarJuego_amigo');
+let boton_agregar = document.getElementById('agregar');
+let campo_lista = document.getElementById('listaAmigos');
+let campo_resultado = document.getElementById('resultado');
+let campo_entrada_texto = document.getElementById('amigo');
 
 //capturar los nombres
 function agregarAmigo(){
-    document.getElementById('boton_sortear').removeAttribute('disabled');
-    nombreDeAmigo = document.getElementById('amigo').value;
+    boton_sortear.removeAttribute('disabled');
+    nombreDeAmigo = campo_entrada_texto.value;
     if(nombreDeAmigo != ''){
-        let amigos = document.getElementById('listaAmigos');
-        amigos.textContent = nombreDeAmigo;        
+        campo_lista.textContent = nombreDeAmigo;        
         listaDeAmigos.push(nombreDeAmigo);
         
         listarAmigos();
@@ -19,19 +24,17 @@ function agregarAmigo(){
 }
 
 function limpiarCampo() {
-    let campo = document.getElementById('amigo');
-    campo.value = '';
+    campo_entrada_texto.value = '';
 }
 
 //listar los nombres
 function listarAmigos() {
-    let listado = document.getElementById('listaAmigos');
-    listado.innerHTML = '';
+    campo_lista.innerHTML = '';
     listaDeAmigos.forEach(
         amigo =>{
             let listaAmigos = document.createElement('li');
             listaAmigos.textContent = amigo;
-            listado.appendChild(listaAmigos);
+            campo_lista.appendChild(listaAmigos);
         }
     )
 }
@@ -43,32 +46,26 @@ function elegirAmigo(lista) {
 //presentar el nombre elegido
 function sortearAmigo() {
     let amigo = elegirAmigo(listaDeAmigos);
-    let resultado = document.getElementById('resultado');
-    let listaAmigos = document.getElementById('listaAmigos');
     if(listaDeAmigos.length > 1 ){
-        resultado.textContent = `Su amigo secreto es: ${amigo}`;
-        document.getElementById('reiniciarJuego_amigo').removeAttribute('disabled');
-        document.getElementById('boton_sortear').disabled = true;
-        document.getElementById('agregar').disabled = true;
-        listaAmigos.innerHTML = '';
+        campo_resultado.textContent = `Su amigo secreto es: ${amigo}`;
+        boton_reinicio.removeAttribute('disabled');
+        boton_sortear.disabled = true;
+        boton_agregar.disabled = true;
+        campo_lista.innerHTML = '';
         listaDeAmigos = [];
     }else{
         alert('favor ingresar al menos dos nombres de amigos');
     }
-
 }
 //reiniciar el juego
 function condicionInicial(){
-    let listaAmigos = document.getElementById('listaAmigos');
-    let listaResultado = document.getElementById('resultado');
-    listaAmigos.innerHTML = '';
-    listaResultado.innerHTML = '';
+    campo_lista.innerHTML = '';
+    campo_resultado.innerHTML = '';
     listaDeAmigos = [];
 }
-
 function reiniciarJuegoAmigo(){
-    document.getElementById('agregar').disabled = false;
+    boton_agregar.disabled = false;
     limpiarCampo();
     condicionInicial();
-    document.querySelector('#reiniciarJuego_amigo').setAttribute('disabled', true);
+    boton_reinicio.setAttribute('disabled', true);
 }
